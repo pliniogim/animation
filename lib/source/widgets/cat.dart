af2f13628aae23bed967f1659f11a2ea47efc605
pliniogim <pliniogimenez@gmail.com>
2022-12-28 20:50:10 -0300

    Add Class Cat

---
+++ lib/source/widgets/cat.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+  const Cat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://i.imgur.com/QwhZRyL.png',
+    );
+
+    ///!https://i.imgur.com/QwhZRyL.png
+  }
+}
